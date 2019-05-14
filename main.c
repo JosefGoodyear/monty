9e96d36d2df9c5187fc6845ce16155738df52c94
jembantonio <jembantonio@gmail.com>
2019-05-14 20:32:35 +0000

    added tokenizing function

--- main.c
+++ main.c
@@ -6,12 +6,28 @@ int main(int argc, char **argv)
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t line_size;
+	char *token = NULL;
+	char *global = NULL;
+
 	file1 = fopen(argv[1], "r");	
 	line_size = getline(&line, &len, file1);
+	token = strtok(line, " ");
+	if (strcmp(token, "push") == 0)
+		global = strtok(NULL, " ");
+	printf("global = %s\n", global);
 	while (line_size >= 0)
-	{
-		printf("%s", line);
+	{	
+		printf("%s\n", token);
 		line_size = getline(&line, &len, file1);
+		token = strtok(line, " ");
+		if (strcmp(token, "push") == 0)
+			global = strtok(NULL, " ");
+		printf("global = %s\n", global);
 	}
+
+	(void)argc;
 	return (0);
 }
+
+
+
