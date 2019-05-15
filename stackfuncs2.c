ca18ea1cb4309e845c2f83b382bee6f40d66cd04
jembantonio <jembantonio@gmail.com>
2019-05-15 21:35:11 +0000

    pop, swap, add functionality

--- stackfuncs2.c
+++ stackfuncs2.c
@@ -2,23 +2,46 @@
 
 void pop(stack_t **stack, unsigned int line_number)
 {
-	(void)stack;
+	stack_t *temp;
+
+	temp = *stack;
+	*stack = temp->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	free(temp);
+
 	(void)line_number;
-	printf("pop\n");
 }
 
 void swap(stack_t **stack, unsigned int line_number)
 {
-	(void)stack;
+	stack_t *curr, *temp;
+	
+	curr = *stack;
+	temp = (*stack)->next;
+
+	if (temp->next != NULL)
+		temp->next->prev = curr;
+	curr->next = temp->next;
+	temp->next = curr;
+	temp->prev = NULL;
+	*stack = temp;
+
 	(void)line_number;
-	printf("swap\n");
 }
 
 void add(stack_t **stack, unsigned int line_number)
 {
-	(void)stack;
+	int sum;
+
+	(*stack) = (*stack)->next;
+
+	sum = (*stack)->n + (*stack)->prev->n;
+	(*stack)->n = sum;
+       	free((*stack)->prev);
+	(*stack)->prev = NULL;
+	
 	(void)line_number;
-	printf("add\n");
 }
 
 void nop(stack_t **stack, unsigned int line_number)
