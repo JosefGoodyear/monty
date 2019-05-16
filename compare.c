599331c49becf48144b6c5a1600585be0ddb15cc
jembantonio <jembantonio@gmail.com>
2019-05-16 17:02:34 +0000

    added descriptions for the compare function

--- compare.c
+++ compare.c
@@ -1,5 +1,16 @@
 #include "monty.h"
 
+/**
+ * compare - a function that holds the function pointer struct and
+ * compares the string from getline to the appropriate opcode
+ * then runs the appropriate function
+ * @token: the tokenized string from textfile
+ * @stack: a pointer to the head of the stack_t
+ * @line_number: the line number of the opcode
+ *
+ * Return: void
+ */
+
 void compare(char *token, stack_t **stack, unsigned int line_number)
 {
 	int i, foundMatch = 0;
